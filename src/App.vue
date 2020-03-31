d3b8387e5cd7f9e6666d3342f93ef734424bd360
Lbo <42620993+l-bolz@users.noreply.github.com>
2020-03-31 15:46:02 +0200

    Added basic Structure

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img
+      alt="D20 Dice"
+      src="./assets/logo.png"
+    >
+    <p>Work in progress</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  assetsDir: 'assets',
+  runtimeCompiler: true
+};
