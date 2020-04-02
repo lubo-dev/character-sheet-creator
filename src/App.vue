c1f2513c7b1ef7a2c5c476f4a8439c7fbeea3f65
Lbo <42620993+l-bolz@users.noreply.github.com>
2020-04-02 16:52:28 +0200

    Extracts v-footer into its own component

--- src/App.vue
+++ src/App.vue
@@ -4,33 +4,20 @@
 
     <document />
 
-    <v-footer
-      color="red darken-3"
-      app
-    >
-      <a
-        href="https://github.com/L-Bolz/character-sheet-creator"
-        class="white--text"
-      >About</a>
-      <span class="white--text">&copy; 2020</span>
-    </v-footer>
+    <c-footer />
   </v-app>
 </template>
 
 <script>
   import NavBar from "./components/NavBar";
   import Document from "./components/Document"
+  import CFooter from "./components/Footer";
 
   export default {
     components: {
       NavBar,
-      Document
+      Document,
+      CFooter
     }
   }
 </script>
-
-<style lang="scss" scoped>
-  footer a {
-    margin-right: 10px;
-  }
-</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-footer
+    color="red darken-3"
+    app
+  >
+    <a
+      href="https://github.com/L-Bolz/character-sheet-creator"
+      class="white--text"
+    >About</a>
+    <span class="white--text">&copy; 2020</span>
+  </v-footer>
+</template>
+
+<style lang="scss" scoped>
+    footer a {
+        margin-right: 10px;
+    }
+</style>
