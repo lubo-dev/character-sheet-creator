1162bec69ec9777b9006995fda89c6b046c1cd71
Lbo <42620993+l-bolz@users.noreply.github.com>
2020-04-02 17:14:19 +0200

    Adds basic A4 page

--- src/components/Document.vue
+++ src/components/Document.vue
@@ -11,7 +11,18 @@
         <v-col class="text-center">
           <p>Work in progress</p>
         </v-col>
+        <v-col >
+          <div class="page" />
+        </v-col>
       </v-row>
     </v-container>
   </v-content>
 </template>
+
+<style lang="scss" scoped>
+  .page {
+    box-shadow: 0 0 3px #555555;
+    width: 595pt;
+    height: 842pt;
+  }
+</style>
