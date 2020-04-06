ddc59c2bbd77021aa64dd352e537d88ac0a43cde
Lbo <42620993+l-bolz@users.noreply.github.com>
2020-04-06 13:43:11 +0200

    Replaced custom style with vuetify class

--- src/components/structure/Footer.vue
+++ src/components/structure/Footer.vue
@@ -5,14 +5,8 @@
   >
     <a
       href="https://github.com/L-Bolz/character-sheet-creator"
-      class="white--text"
+      class="white--text mr-3"
     >About</a>
     <span class="white--text">&copy; 2020</span>
   </v-footer>
 </template>
-
-<style lang="scss" scoped>
-    footer a {
-        margin-right: 10px;
-    }
-</style>
